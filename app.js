e8dc4d085a0140dab1f42566e22dd853d024941a
Levi <levi_blodgett@outlook.com>
2019-02-06 21:58:17 -0600

    Learned about observer pattern, VERY cool

--- app.js
+++ app.js
@@ -141,4 +141,65 @@ members.push(factory.createMember('Will Thomas', 'standard'));
 
 members.forEach(function(member) {
   member.define();
-})
+});
+
+// OBSERVER PATTERN
+
+function EventObserver() {
+  this.observers = [];
+};
+
+EventObserver.prototype = {
+  subscribe: function(fn) {
+    this.observers.push(fn);
+    console.log(`You are now subscribed to ${fn.name}.`);
+  },
+  unsubscribe: function(fn) {
+    // Filter out from the list whatever matches the callback function. IF there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of observers.
+    this.observers = this.observers.filter(function(item) {
+      if (item !== fn) {
+        return item;
+      }
+    });
+    console.log(`You are now unsubscribred from ${fn.name}.`);
+  },
+  fire: function() {
+    this.observers.forEach(function(item) {
+      item.call();
+    })
+  }
+};
+
+const click = new EventObserver();
+
+// Event listeners for milliseconds
+document.querySelector('.sub-ms').addEventListener('click', function() {
+  click.subscribe(getCurMilliseconds);
+});
+
+document.querySelector('.unsub-ms').addEventListener('click', function() {
+  click.unsubscribe(getCurMilliseconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function() {
+  click.fire();
+});
+
+// Click Handler for milliseconds
+const getCurMilliseconds = function() {
+  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+};
+
+// Event listeners for seconds
+document.querySelector('.sub-s').addEventListener('click', function() {
+  click.subscribe(getCurSeconds);
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function() {
+  click.unsubscribe(getCurSeconds);
+});
+
+// Click Handler for seconds
+const getCurSeconds = function() {
+  console.log(`Current Seconds: ${new Date().getSeconds()}`);
+};
